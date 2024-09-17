e72a14753ad6a3651cd02e996f134ac324de64bb
JulesBestDev176 <souleymanefall176@gmail.com>
2024-09-17 02:02:33 +0000

    deploiement

---
+++ backend/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import productRoute from "./routes/product.route.js";
+import path from "path";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+app.use(express.json()); // allows us to accept JSON data in the body
+
+app.use("/api/products", productRoute);
+
+if (process.env.Node_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(5000, () => {
+  connectDB();
+  console.log("Server started at http://localhost:" + PORT);
+});
